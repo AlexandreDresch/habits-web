61e4fc264f8755aa4cfd3b96d58fb90127cd9fa5
AlexandreDresch <z3r0pc@protonmail.com>
2023-02-03 23:40:27 -0300

    feat: Created global Tailwind configuration & Header component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./styles/global.css";
+
+import { Habit } from "./components/Habit/Habit";
+import { Header } from "./components/Header/Header";
+
+export default function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex-col gap-16">
+        <Header />
+      </div>
+    </div>
+  );
+}
